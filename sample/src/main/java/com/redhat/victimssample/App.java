70fe50ac6dd1d2b1bb8d2c919224e4356c4b0da3
David.jorm <djorm@redhat.com>
2013-02-13 13:58:38 +1000

    Added sample application that uses the enforce-victims-rule plugin

---
+++ sample/src/main/java/com/redhat/victimssample/App.java
@@ -0,0 +1,13 @@
+package com.redhat.com.srt.sample;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
