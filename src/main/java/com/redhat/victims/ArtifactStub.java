10758d9984449a79b19ce9e3795de4fbf87fd4ad
Grant Murphy <grantcmurphy@gmail.com>
2013-05-31 17:25:57 +1000

    Actually adding license to headers

--- src/main/java/com/redhat/victims/ArtifactStub.java
+++ src/main/java/com/redhat/victims/ArtifactStub.java
@@ -1,5 +1,26 @@
 package com.redhat.victims;
 
+/*
+ * #%L
+ * This file is part of victims-enforcer.
+ * %%
+ * Copyright (C) 2013 The Victims Project
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * #L%
+ */
+
 import java.io.IOException;
 import java.io.Serializable;
 import java.util.Date;
